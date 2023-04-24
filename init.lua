2a14a0129e4143e7da04e69da0a517c1a1aa1546
Nils Jeisecke <nils.jeisecke@saltation.com>
2023-04-24 11:35:34 +0200

    remove deprecated nvim-tree 'mapping' config

--- init.lua
+++ init.lua
@@ -215,11 +215,6 @@ require('nvim-tree').setup {
     sort_by = "case_sensitive",
     view = {
         adaptive_size = false,
-        mappings = {
-            list = {
-                { key = "u", action = "dir_up" },
-            },
-        },
     },
     renderer = {
         group_empty = true,
