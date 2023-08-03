ed1aa53cc2937ecd5247f79d22ffb3cf05fccaed
Nils Jeisecke <nils.jeisecke@saltation.com>
2023-08-03 15:38:45 +0200

    enable clippy warnings

--- init.lua
+++ init.lua
@@ -322,6 +322,17 @@ rt.setup({
             -- Code action groups
             vim.keymap.set("n", "<Leader>a", rt.code_action_group.code_action_group, { buffer = bufnr })
         end,
+
+        settings = {
+            -- to enable rust-analyzer settings visit:
+            -- https://github.com/rust-analyzer/rust-analyzer/blob/master/docs/user/generated_config.adoc
+            ["rust-analyzer"] = {
+                -- enable clippy on save
+                checkOnSave = {
+                    command = "clippy",
+                },
+            },
+        },
     },
 })
 
