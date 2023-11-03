ab21f7193994d6738852757b7611fcb77c95b20f
Nils Jeisecke <nils.jeisecke@saltation.com>
2023-11-03 16:50:16 +0100

    fix ibl

--- init.lua
+++ init.lua
@@ -175,23 +175,7 @@ vim.cmd [[highlight IndentBlanklineIndent4 guifg=#2d3033 gui=nocombine]]
 vim.cmd [[highlight IndentBlanklineIndent5 guifg=#2d3033 gui=nocombine]]
 vim.cmd [[highlight IndentBlanklineIndent6 guifg=#2d3033 gui=nocombine]]
 
-require("indent_blankline").setup {
-    -- char = '┊',
-    char = ' ',
-    use_treesitter = true,
-    use_treesitter_scope = true,
-    show_first_indent_level = true,
-    space_char_blankline = " ",
-    char_highlight_list = {
-        "IndentBlanklineIndent1",
-        "IndentBlanklineIndent2",
-        "IndentBlanklineIndent3",
-        "IndentBlanklineIndent4",
-        "IndentBlanklineIndent5",
-        "IndentBlanklineIndent6",
-    },
-}
-
+require("ibl").setup()
 
 -- Todo Comments Setup
 require('todo-comments').setup {
